a498395df3b79f6e38be279d6186955b2df5bc76
mik3612 - M. Vedel <mikkelvedel@live.com>
2017-12-11 17:55:31 +0100

    Version 1.0.5

--- Console/Program.cs
+++ Console/Program.cs
@@ -24,50 +24,14 @@ namespace Domitus
             Stopwatch watch = new Stopwatch();
             watch.Start();
 
-            Console.WriteLine("Trying to take screen..");
             actions.Screenshot();
-            Console.WriteLine("Took screen");
-            Console.WriteLine("Converting image to bytes..");
-            Image img = Image.FromFile("Screenshot.png");
-            Console.WriteLine("Converting image to bytes..");
-            Console.WriteLine("Trying to save array as file..");
-            saveByteArrayToFile(imageToByteArray(img));
-            Console.WriteLine("Saved as bytes..");
-            Console.WriteLine("Trying to convert bytes to image..");
-            Image convertingImage = byteArrayToImage(getByteArrayFromFile("Temp.txt"));
-            convertingImage.Save("Screenshot2.png");
-            Console.WriteLine("Done");
-
-
+            
             //Skriv hvor lang tid det har taget at køre koden
             watch.Stop();
             string time = watch.Elapsed.TotalSeconds.ToString();
             Console.WriteLine("Took: " + time);
             Console.ReadKey();
         }
-        public byte[] imageToByteArray(System.Drawing.Image imageIn)
-        {
-            MemoryStream ms = new MemoryStream();
-            imageIn.Save(ms, System.Drawing.Imaging.ImageFormat.Gif);
-            return ms.ToArray();
-        }
 
-        public Image byteArrayToImage(byte[] byteArrayIn)
-        {
-            MemoryStream ms = new MemoryStream(byteArrayIn);
-            Image returnImage = Image.FromStream(ms);
-            return returnImage;
-        }
-
-        public void saveByteArrayToFile(byte[] array)
-        {
-            File.WriteAllBytes("Temp.txt", array);
-        }
-
-        public byte[] getByteArrayFromFile(string directory)
-        {
-            byte[] file = File.ReadAllBytes(directory);
-            return file;
-        }
     }
 }
--- DomitusJM/Actions/Screenshot.cs
+++ DomitusJM/Actions/Screenshot.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.IO;
 
 namespace Domitus
 {
@@ -31,6 +32,44 @@ namespace Domitus
 
             // Save the screenshot to the specified path that the user has chosen.
             bmpScreenshot.Save("Screenshot.png", ImageFormat.Png);
+
+            //Converting image to bytes
+            Image img = Image.FromFile("Screenshot.png");
+
+            //Saving to byte array
+            saveByteArrayToFile(imageToByteArray(img));
+
+            //Converting bytes to image
+            Image convertingImage = byteArrayToImage(getByteArrayFromFile("Temp.txt"));
+
+            //Save new converted image
+            convertingImage.Save("Screenshot2.png");
+        }
+
+        public static byte[] imageToByteArray(System.Drawing.Image imageIn)
+        {
+            MemoryStream ms = new MemoryStream();
+            imageIn.Save(ms, System.Drawing.Imaging.ImageFormat.Gif);
+            return ms.ToArray();
+
+        }
+
+        public static Image byteArrayToImage(byte[] byteArrayIn)
+        {
+            MemoryStream ms = new MemoryStream(byteArrayIn);
+            Image returnImage = Image.FromStream(ms);
+            return returnImage;
+        }
+
+        public static void saveByteArrayToFile(byte[] array)
+        {
+            File.WriteAllBytes("Temp.txt", array);
+        }
+
+        public static byte[] getByteArrayFromFile(string directory)
+        {
+            byte[] file = File.ReadAllBytes(directory);
+            return file;
         }
     }
 }
